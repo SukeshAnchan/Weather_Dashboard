177fe355187df1a9e8e4ec2d08d90554833e278d
David Schepis <davidschepis@gmail.com>
2021-12-16 20:23:29 -0700

    search history now populates

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,7 +34,21 @@ function GetSearchHistory() {
     return JSON.parse(localStorage.getItem("history"));
 }
 function PopulateSearchHistory() {
-
+    var history = [].concat(GetSearchHistory());
+    if (history[0] === null) {
+        return;
+    }
+    var searchMenu = $('#searchMenu');
+    $('.searchButton').remove();
+    for (var i = history.length-1; i >= 0; i--) {
+        var button = $('<button>');
+        button.addClass("btn");
+        button.addClass("btn-secondary");
+        button.addClass("btn-block");
+        button.addClass("searchButton");
+        button.text(history[i]);
+        searchMenu.append(button);
+    }
 }
 
 
