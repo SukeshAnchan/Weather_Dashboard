adc18093452ede1cd004270adb6eeb808e9b9626
David Schepis <davidschepis@gmail.com>
2021-12-16 19:33:34 -0700

    clicking search now saves the entry into local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 
 PopulateSearchHistory();
 
-$('#searchButton').click(function() {
+$('#searchButton').click(function () {
     var weatherData = GetWeatherData($('#searchBox').val());
     SetWeatherData(weatherData);
     //console.log(weatherData + " **\n" + $('#searchBox').val());
@@ -20,10 +20,18 @@ function SetWeatherData(weatherData) {
 
 function AddSearchHistory(city) {
     var history = GetSearchHistory();
+    if (history === null) {
+        localStorage.setItem("history", JSON.stringify(city));
+    }
+    else {
+        var historySave = [].concat(history);
+        historySave.push(city);
+        localStorage.setItem("history", JSON.stringify(historySave));
+    }
 }
 
 function GetSearchHistory() {
-
+    return JSON.parse(localStorage.getItem("history"));
 }
 function PopulateSearchHistory() {
 
